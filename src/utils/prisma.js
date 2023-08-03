d1c6dbeead764f38c7bd4b69ce78da0e03a1a31a
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-08-03 23:05:58 +0700

    fix: deployment prisma client

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,2 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+
+const globalForPrisma = globalThis;
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
