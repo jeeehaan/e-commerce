c84e346215a0f1257a020e887c8731587073ee3b
Afifah <111559790+mbkfah@users.noreply.github.com>
2023-08-05 05:57:33 +0700

    feat: added login API

--- src/app/api/v1/auth/login/route.js
+++ src/app/api/v1/auth/login/route.js
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
-
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { cookies } from "next//headers";
 export async function POST(request) {
   const { email, password } = await request.json();
 
@@ -12,11 +14,23 @@ export async function POST(request) {
     });
 
     if (findUser) {
-      const isPasswordMatch = password === findUser.password;
+      const isPasswordMatch = await compare(password, findUser.password);
 
-      if (isPasswordMatch)
-        return NextResponse.json({ data: findUser }, { status: 200 });
+      if (isPasswordMatch) {
+        const payload = {
+          id: findUser.id,
+          name: findUser.name,
+          email: findUser.email,
+          username: findUser.username,
+          bio: findUser.bio,
+        };
 
+        const accessToken = sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "7d",
+        });
+        cookies().set("accessToken", accessToken);
+        return NextResponse.json({ data: payload }, { status: 200 });
+      }
       return NextResponse.json(
         { error: "Your password is incorrect" },
         { status: 401 }
