212c390a0fe0ea3395ac4244bca8bc1403b1cb9b
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-08-11 22:44:39 +0700

    feat: Home UI retouch

--- src/app/dashboard/product/create/page.js
+++ src/app/dashboard/product/create/page.js
@@ -2,7 +2,7 @@ import { CreateProduct } from "@/components/Dashboard/Components/CreateProduct";
 import { API_URL } from "@/config/apiUrl";
 
 async function getCategories() {
-  const res = await fetch(`${API_URL}/category`);
+  const res = await fetch(`${API_URL}/category`, { cache: "no-store" });
   const data = await res.json();
   return data;
 }
