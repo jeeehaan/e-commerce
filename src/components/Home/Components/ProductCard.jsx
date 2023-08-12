850dee9f44e610651e4fe34b788a8c2ce6467682
Jessy Hanifiah <72622302+jeeehaan@users.noreply.github.com>
2023-08-12 18:22:58 +0700

    feat: update hrefs

--- src/components/Home/Components/ProductCard.jsx
+++ src/components/Home/Components/ProductCard.jsx
@@ -10,7 +10,7 @@ export const ProductCard = ({ product }) => {
     <Link href={`/product/${product.slug}`}>
       <div className="bg-purple-200 rounded-xl cursor-pointer">
         <div className="hover:scale-105 transition duration-300 ease-in-out">
-          <Image src={`${imageURL}/${product.id}/${product.featuredImage}`} width={400} height={400} className="rounded-2xl" />
+          <Image src={`${imageURL}/${product.id}/${product.featuredImage}`} width={400} height={400} className="rounded-2xl" alt="Product Image" />
         </div>
         <div className="flex justify-between p-4 text-sm">
           <div className="text-black font-medium">{trimString(product.name, 30)}</div>
--- src/components/Home/Templates/Header.jsx
+++ src/components/Home/Templates/Header.jsx
@@ -1,17 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
   return (
     <div className="flex items-center justify-between p-6">
       <div className="flex gap-12">
-        <div className="font-bold ">Ascendancy</div>
-        <div>Browse All</div>
+        <Link href="/" className="font-bold ">
+          Ascendancy
+        </Link>
+        <Link href="/">Browse All</Link>
       </div>
       <div className="flex items-center gap-12">
-        <div>Sign In</div>
-        <div>
+        <Link href="/login">Sign In</Link>
+        <Link href="/login">
           <button>Get Started</button>
-        </div>
+        </Link>
       </div>
     </div>
   );
